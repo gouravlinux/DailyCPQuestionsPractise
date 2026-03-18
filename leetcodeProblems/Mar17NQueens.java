86cd6414a2f08a4669a856ef5b92063472347061
gouravlinux <2005gouravsoni@gmail.com>
2026-03-18 07:37:18 +0530

    An optimized version of N-Queens

--- leetcodeProblems/Mar17NQueens.java
+++ leetcodeProblems/Mar17NQueens.java
@@ -52,3 +52,74 @@ class Solution {
         return res;
     }
 }
+class Solution {
+    List<List<String>> res;
+    Set<Integer> colSet;
+    Set<Integer> diagonalSet;
+    Set<Integer> antiDiagonalSet;
+    int[] columnArr;
+
+    private boolean clash(int row, int col, int n) {
+        if (colSet.contains(col)) {//check if in same upward column any other queen is present or not
+            return true;
+        }
+        if (diagonalSet.contains(row + col)) {//check if in right upward diagonal queen is present or not
+            return true;
+        }
+        if (antiDiagonalSet.contains(row - col)) {//check if in left upward diagonal queen is present or not
+            return true;
+        }
+        return false;
+
+    }
+
+    private List<String> makeString(int n) {
+        List<String> temp = new ArrayList<>();
+        for (int row = 0; row < n; row++) {
+            StringBuilder newString = new StringBuilder();
+            for (int column = 0; column < n; column++) {
+                if (column == columnArr[row]) {
+                    newString.append('Q');
+                    continue;
+                }
+                newString.append('.');
+            }
+            temp.add(newString.toString());
+        }
+        return temp;
+    }
+
+    private void solve(int row, int n) {
+        if (row == n) {
+            List<String> temp = makeString(n);
+            res.add(temp);
+            return;
+        }
+        for (int col = 0; col < n; col++) {
+            if (clash(row, col, n)) {
+                continue;
+            }
+            // else
+            colSet.add(col);//include
+            diagonalSet.add(row + col);//right diagonal = coordinates sum = (row+col)
+            antiDiagonalSet.add(row - col);//left diagonal = coordinates difference = (row-col)
+            columnArr[row] = col;
+            solve(row + 1, n);//explore
+            //exclude
+            colSet.remove(col);
+            diagonalSet.remove(row + col);
+            antiDiagonalSet.remove(row - col);
+            // now exploration done through for loop
+        }
+    }
+
+    public List<List<String>> solveNQueens(int n) {
+        colSet = new HashSet<>();
+        diagonalSet = new HashSet<>();
+        antiDiagonalSet = new HashSet<>();
+        columnArr = new int[n];
+        res = new ArrayList<>();
+        solve(0, n);
+        return res;
+    }
+}
